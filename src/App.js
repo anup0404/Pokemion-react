1528bf16bec496c5f53ca8924d224ecd75e919ca
anup0404 <anupkumarggupta@gmail.com>
2023-01-22 18:40:09 +0530

    project done

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import PokemonInfo from "./components/PokemonInfo";
+import PokemonSearch from "./components/PokemonSearch";
+import PokemonTable from "./components/PokemonTable";
+function App() {
+  console.log("app rendered");
+  const [filter, setFilter] = useState("");
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    console.log("useeffect run");
+    fetch("pokemon.json")
+      .then((response) => response.json())
+      .then((data) => setPokemon(data))
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+
+  return (
+    <div>
+      style=
+      {{
+        margin: "auto",
+        width: 800,
+        paddingTop: "1em",
+      }}
+      <h1 className="title">Pokemon Search</h1>
+      <div>
+        style=
+        {{
+          display: "grid",
+          gridTemplateColumns: "80% 20%",
+          gridColumnGap: "1em",
+        }}
+        <div>
+          <PokemonSearch filter={filter} setFilter={setFilter} />
+          <PokemonTable
+            pokemon={pokemon}
+            filter={filter}
+            setSelectedPokemon={setSelectedPokemon}
+          />
+        </div>
+        {selectedPokemon && <PokemonInfo {...selectedPokemon} />}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PokemonSearch.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function PokemonSearch({ filter, setFilter }) {
+  const searchPokemon = (event) => {
+    setFilter(event.target.value);
+  };
+  return <input type="text" value={filter} onChange={searchPokemon} />;
+}
+
+export default PokemonSearch;
